01cdd288f1fe239ed2c8baa548c576684f5cc266
Taras <murzenkovtn@gmail.com>
2015-12-16 18:43:36 +0200

    after merging

--- src/main/java/model/ReplyListener.java
+++ src/main/java/model/ReplyListener.java
@@ -6,9 +6,4 @@ import java.io.IOException;
 
 public interface ReplyListener {
     void onReply(Message message) throws IOException;
-<<<<<<< HEAD
-    //dsfgsdfg
-=======
-    // fake commit
->>>>>>> origin/master
 }
